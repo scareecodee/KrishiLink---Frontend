323405eb392be54bb07be56dd2b1b7d4471713dc
sundram kumar <sundramkumar12345890@gmail.com>
2026-09-06 00:13:13 +0530

    update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.API_URL || '/api';
+const API_BASE_URL = import.meta.env.API_URL;
 
 const api = axios.create({ 
   baseURL: API_BASE_URL,
